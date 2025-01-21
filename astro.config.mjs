d1b2917d47fac6ca279c3dc4e2bc5bcacaf7f444
inkarri <jhanpieremontes@gmail.com>
2025-01-20 23:27:25 -0500

    Se agrego los archivos para el build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://inkarr1.github.io/personal-website/',
+    base: '/personal-website/',
+});
