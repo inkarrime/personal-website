7e0865d481753dc826a086d249e7a9a6551a70db
inkarri <jhanpieremontes@gmail.com>
2025-01-21 10:26:18 -0500

    Se configuro para redireccionar a inkarri.me

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://inkarr1.github.io/personal-website',
-    base: '/personal-website/',
+    site: 'https://inkarri.me',
+    base: '/',
 });
