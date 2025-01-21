7a6d4fe983ceb4ecb38d2ee1aa107f5975272422
inkarri <jhanpieremontes@gmail.com>
2025-01-21 09:22:21 -0500

    Se mejoraron la vista de version mobile

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://inkarr1.github.io/personal-website/',
+    site: 'https://inkarr1.github.io/personal-website',
     base: '/personal-website/',
 });
